b856d5a56dcc7ef36f6daa5e08f5b04ba5fd6a7c
Dmitry <dsilichev@gmail.com>
2024-06-05 17:43:25 +0300

    HOC

--- src/App.js
+++ src/App.js
@@ -1,6 +1,31 @@
 import { Component, useEffect, useState } from 'react';
 import styles from './app.module.css';
 
+// HOC High order component
+const withLogging = (Component) => {
+  const NewComponent = (props) => {
+    console.log(props.user);
+
+    return <Component {...props} />;
+  };
+
+  return NewComponent;
+};
+
+const withLoggingAndColor = (Component, color) => {
+  const NewComponent = (props) => {
+    console.log(props.user);
+
+    return (
+      <span style={{ color }}>
+        <Component {...props} />
+      </span>
+    );
+  };
+
+  return NewComponent;
+};
+
 export const HelloMessage = ({ user }) => {
   return <span>Привет, {user}</span>;
 };
@@ -9,14 +34,42 @@ export const GoodByeMessage = ({ user }) => {
   return <span>До свидания, {user}</span>;
 };
 
+const HelloMessageWithLogging = withLogging(HelloMessage);
+const HelloMessageWithLoggingAndColor = withLoggingAndColor(HelloMessage, 'red');
+
+const MessageWithLoggingAndColor = ({ Message, color, ...props }) => {
+  console.log(props.user);
+
+  return (
+    <span style={{ color }}>
+      <Message {...props} />
+    </span>
+  );
+};
+
+const ComponentWithLoggingAndColor = ({ children, color, ...props }) => {
+  console.log(props.user);
+
+  return <span style={{ color }}>{children}</span>;
+};
+
 // Render Props
-export const UserWidget = ( { render }) => {
+export const UserWidget = ({ render }) => {
   const user = 'Vasya';
   return (
     <div>
       <div>Текущий пользователь: {user}</div>
       <div>Сообщение:</div>
-      {render(user)}
+      <HelloMessageWithLogging user={user} />
+      <br />
+      <HelloMessageWithLoggingAndColor user={user} />
+      <br />
+      <MessageWithLoggingAndColor Message={HelloMessage} user={user} color="green" />
+      <br />
+      <ComponentWithLoggingAndColor color="blue" user={user}>
+        <HelloMessage user={user} />
+      </ComponentWithLoggingAndColor>
+      {/* {render(user)} */}
       {/* <HelloMessage user={user} />
       <GoodByeMessage user={user} /> */}
     </div>
@@ -38,8 +91,9 @@ function App({ message }) {
 
   return (
     <div className={styles.app}>
-      <UserWidget render={(user) => <HelloMessage user={user}/>} />
-      <UserWidget render={(user) => <GoodByeMessage user={user}/>} />
+      <UserWidget />
+      {/* <UserWidget render={(user) => <HelloMessage user={user}/>} /> */}
+      {/* <UserWidget render={(user) => <GoodByeMessage user={user}/>} /> */}
       <div>
         {message}: {screenWidth}
       </div>
